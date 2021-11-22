eda3f9078558ae67fb0144506983dab31950e493
Osias Miranda <osiasmiranda@gmail.com>
2021-11-22 15:13:27 -0400

    testando order

--- src/modules/store/store.module.ts
+++ src/modules/store/store.module.ts
@@ -1,13 +1,17 @@
+import { OrderItem } from 'src/modules/store/entities/order-item.entity';
+import { Order } from './entities/order.entity';
 import { OrderService } from './services/order.service';
 import { ProductController } from './controllers/product.controller';
 import { ProductService } from './services/product.service';
 import { Product } from 'src/modules/store/entities/product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { OrderItemService } from './services/order-item.service';
+import { OrderController } from './controllers/order.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
-  providers: [ProductService, OrderService],
-  controllers: [ProductController]
+  imports: [TypeOrmModule.forFeature([Product, Order, OrderItem])],
+  providers: [ProductService, OrderService, OrderItemService],
+  controllers: [ProductController, OrderController]
 })
 export class StoreModule {}
